cefbe33ecb9c14eb0232e3a18dbfbf4f7bbb3c59
iramos <iramos@106-24.info2>
2022-09-21 13:35:30 +0200

    Segundo commit

---
+++ EjerciciosBasicos/src/ejerciciosbasicos/Ejercicio3.java
@@ -0,0 +1,10 @@
+package ejerciciosbasicos;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// Escribir una aplicación que pida el año actual y el año de nacimiento del usuario. Debe calcular su edad.
+
+	}
+
+}
