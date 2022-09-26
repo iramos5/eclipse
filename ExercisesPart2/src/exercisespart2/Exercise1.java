a975845fb027ffd2ced9321b272859345de5c52b
iramos <iramos@106-24.info2>
2022-09-26 09:53:57 +0200

    exercises part 2

---
+++ ExercisesPart2/src/exercisespart2/Exercise1.java
@@ -0,0 +1,11 @@
+package exercisespart2;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ejercicio1");
+
+	}
+
+}
