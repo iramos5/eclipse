6256a5587c98724de21ecff8c29840d194b797f7
iramos <iramos@106-24.info2>
2022-09-26 09:59:02 +0200

    exercise 1

--- ExercisesPart2/src/exercisespart2/Exercise1.java
+++ ExercisesPart2/src/exercisespart2/Exercise1.java
@@ -1,11 +1,21 @@
 package exercisespart2;
+import java.util.Scanner;
 
 public class Exercise1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ejercicio1");
+		Scanner keyboard = new Scanner(System.in);
+		final int YEAR = 2022;
+		int age, bornIn;
+		
+		System.out.print("How old are you this year? ");
+		age = keyboard.nextInt();
+		bornIn = YEAR - age;
+		System.out.println("I think you were born in " + bornIn);
+		keyboard.close();
 
 	}
 
 }
+
