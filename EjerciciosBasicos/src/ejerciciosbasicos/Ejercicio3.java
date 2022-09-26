102c4ac7f87d195b6bdd03ffd19256c64bc888f2
iramos <iramos@106-24.info2>
2022-09-26 08:52:34 +0200

    ejercicio3

--- EjerciciosBasicos/src/ejerciciosbasicos/Ejercicio3.java
+++ EjerciciosBasicos/src/ejerciciosbasicos/Ejercicio3.java
@@ -1,10 +1,23 @@
 package ejerciciosbasicos;
+import java.util.Scanner;
 
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
 		// Escribir una aplicación que pida el año actual y el año de nacimiento del usuario. Debe calcular su edad.
-
+		int actual;
+		int nacimiento;
+		int edad;
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.println("Introduce el año actual: ");
+		actual = scan.nextInt();
+		System.out.println("Introduce tu año de nacimiento: ");
+		nacimiento = scan.nextInt();
+		
+		edad = actual - nacimiento;
+		
+		System.out.println("Tu edad es de " + edad + " años");
 	}
 
 }
